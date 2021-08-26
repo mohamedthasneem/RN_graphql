0994c42718f6889794a6d31ed81d0262a102361a
Thasneem <amythasneem@gmail.com>
2021-08-26 16:20:27 +0530

    search page

---
+++ src/components/KeyboardDismissView.tsx
@@ -0,0 +1,36 @@
+import React, {PropsWithChildren} from 'react';
+import {Keyboard, ScrollView, StyleSheet, TouchableOpacity} from 'react-native';
+
+interface Props {
+  withScrollView?: boolean;
+}
+
+const KeyBoardDismissView: React.FC<Props> = (
+  props: PropsWithChildren<Props>,
+) => {
+  if (props.withScrollView) {
+    return (
+      <ScrollView
+        keyboardShouldPersistTaps="never"
+        contentContainerStyle={s.container}>
+        {props.children}
+      </ScrollView>
+    );
+  }
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      style={s.container}
+      onPress={Keyboard.dismiss}>
+      {props.children}
+    </TouchableOpacity>
+  );
+};
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default KeyBoardDismissView;
--- src/components/Search/SearchScreen.tsx
+++ src/components/Search/SearchScreen.tsx
@@ -1,12 +1,49 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
+import { FlatList, StyleSheet, TextInput } from 'react-native';
 import {Box, Text} from 'react-native-design-utility';
+
+import {theme} from '../../constants/theme';
+import KeyBoardDismissView from '../KeyboardDismissView';
 
 const SearchScreen = () => {
   return (
-    <Box f={1} center>
-      <Text color="red">SearchScreen</Text>
-    </Box>
+    <KeyBoardDismissView>
+
+      <Box f={1} bg="white">
+        <Box h={58} w="100%" px="sm" my="sm">
+          <TextInput style={s.input} placeholder="Search Podcast" selectionColor={theme.color.blueLight}/>
+        </Box>
+
+        <FlatList style={s.list} data={[{id:1},{id:2}]} renderItem={() => (
+            <Box h={98} dir="row" align="center" px="sm">
+              <Box h={78} w={78} bg="blueLight" radius={10} mr={10}/>
+              <Box>
+              <Text bold>Title</Text>
+              <Text size="xs" color="grey">Subtitle of the book</Text>
+              <Text size="xs" color="blueLight">400 episodes</Text>
+              </Box>
+            </Box>
+        )} keyExtractor={(item) => String(item.id)} />
+
+
+      </Box>
+    </KeyBoardDismissView>
   );
 };
+
+const s = StyleSheet.create({
+  input: {
+    height: 40,
+    flex:1,
+    backgroundColor:theme.color.greyLightest,
+    borderRadius:10,
+    paddingHorizontal:theme.space.sm,
+    fontSize:theme.text.size.md,
+  },
+  list:{
+    minHeight:'100%',
+  },
+});
 
 export default SearchScreen;
