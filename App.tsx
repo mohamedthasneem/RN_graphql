6931837d67c2b8e8b0af5b1e39d777109a82be37
Thasneem <amythasneem@gmail.com>
2021-09-21 13:36:48 +0530

    i18n button added

--- App.tsx
+++ App.tsx
@@ -5,12 +5,24 @@ import {NavigationContainer} from '@react-navigation/native';
 import {theme} from './src/constants/theme';
 import MainStackNavigator from './src/navigators/MainStackNavigator';
 
-import './src/i18n/i18n';
+import i18n from './src/i18n/i18n';
+import {View, Button} from 'react-native';
+
+const changeLang = (ln: string) => {
+  return () => {
+    i18n.changeLanguage(ln);
+    console.log('lang change to ' + ln);
+  };
+};
 
 const App = () => {
   return (
     <UtilityThemeProvider theme={theme}>
       <NavigationContainer>
+        <View>
+          <Button title="en" onPress={changeLang('en')} />
+          <Button title="ko" onPress={changeLang('ko')} />
+        </View>
         <MainStackNavigator />
       </NavigationContainer>
     </UtilityThemeProvider>
--- src/components/ListenNow/ListenNowScreen.tsx
+++ src/components/ListenNow/ListenNowScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Box, Text} from 'react-native-design-utility';
+import {useTranslation} from 'react-i18next';
 
 const ListenNowScreen = () => {
+  const {t} = useTranslation();
   return (
     <Box f={1} center>
-      <Text color="red">ListenNowScreen</Text>
+      <Text color="red">{t('lis now')}</Text>
     </Box>
   );
 };
